53c7bcb550d7a9e4f6bb38006bbc8944e19af7cd
Ripon Ghosh <ripon.ghosh200@gmail.com>
2018-09-17 18:52:55 +0600

    basic mail send

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,31 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Mail\SendEmailMailable;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-email', function () {
+		$to = 'ripon.ghosh200@gmail.com';
+	    $from = 'support@shuttlebd.com';
+	    $email = 'ripon.ghosh200@gmail.com';
+	    $name = 'Test';
+	    $subject = 'Test';
+	    $text = 'Test';
+	    $mail_data = [
+	    	'name'    => $name, 
+	    	'text'    => $text,
+	    	'subject' => $subject,
+	    	'email'   => $email,
+	    ];
+	    $mail_send = Mail::send('email.name', $mail_data, function($message) use ($to,$from,$name,$subject) {
+            $message->from($from, $name);
+            $message->to($to);
+            $message->subject($subject);
+        });
+	
+    return 'mail send';
 });
